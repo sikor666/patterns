9a5451819c097bdcb860e9e14bc9af7adf59e638
Tomek <sikor666@gmail.com>
2019-01-23 15:05:13 +0100

    Composite

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 /*
- * Kompozyt – strukturalny wzorzec projektowy, którego celem jest sk³adanie obiektów w taki sposób, aby klient widzia³ wiele z nich jako pojedynczy obiekt.
+ * Kompozyt – strukturalny wzorzec projektowy, którego celem jest sk³adanie obiektów
+ * w taki sposób, aby klient widzia³ wiele z nich jako pojedynczy obiekt.
  *
- * Wzorzec ten stosuje siê, gdy wygodniej jest korzystaæ z pewnych operacji dla danego obiektu w ten sam sposób jak dla grupy obiektów, np. rysuj¹c na ekranie prymitywy lub obiekty z³o¿one z prymitywów; zmieniaj¹c rozmiar zarówno pojedynczych prymitywów jak i obiektów z³o¿onych z prymitywów (z zachowaniem proporcji).
+ * Wzorzec ten stosuje siê, gdy wygodniej jest korzystaæ z pewnych operacji dla
+ * danego obiektu w ten sam sposób jak dla grupy obiektów, np. rysuj¹c na ekranie
+ * prymitywy lub obiekty z³o¿one z prymitywów; zmieniaj¹c rozmiar zarówno
+ * pojedynczych prymitywów jak i obiektów z³o¿onych z prymitywów (z zachowaniem
+ * proporcji).
  */
 
 #include "exercise.h"
