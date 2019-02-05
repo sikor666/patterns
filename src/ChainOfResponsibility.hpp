0d380383e4417c33beb18258df25d1f23cf49d98
Tomek <sikor666@gmail.com>
2019-02-05 18:51:12 +0100

    Chain of Responsibility

--- src/ChainOfResponsibility.hpp
+++ src/ChainOfResponsibility.hpp
@@ -1,10 +1,19 @@
 #pragma once
 
 #include <vector>
-
 using namespace std;
 
-struct Game;
+struct Creature;
+struct Game
+{
+    vector<Creature*> creatures;
+};
+
+struct StatQuery
+{
+    enum Statistic { attack, defense } statistic;
+    int result;
+};
 
 struct Creature
 {
@@ -15,71 +24,54 @@ protected:
 public:
     Creature(Game &game, int base_attack, int base_defense) : game(game), base_attack(base_attack),
         base_defense(base_defense) {}
+
     virtual int get_attack() = 0;
     virtual int get_defense() = 0;
-
-    virtual int attack_modifier() = 0;
-    virtual int defense_modifier() = 0;
-};
-
-struct Game
-{
-    vector<Creature*> creatures;
-
-    int attackModifier(Creature* creature)
-    {
-        int result = 0;
-        for (auto c : creatures)
-        {
-            result += c->attack_modifier();
-        }
-
-        return result - creature->attack_modifier();
-    }
-
-    int defenseModifier(Creature* creature)
-    {
-        int result = 0;
-        for (auto c : creatures)
-        {
-            result += c->defense_modifier();
-        }
-
-        return result - creature->defense_modifier();
-    }
-};
-
-struct StatQuery
-{
-    enum Statistic { attack, defense } statistic;
-    int result;
+    virtual void query(void* source, StatQuery& sq) = 0;
 };
 
 class Goblin : public Creature
 {
+    int get_statistic(StatQuery::Statistic stat)
+    {
+        StatQuery q{ stat,0 };
+        for (auto c : game.creatures)
+            c->query(this, q);
+        return q.result;
+    }
 public:
     Goblin(Game &game, int base_attack, int base_defense) : Creature(game, base_attack, base_defense) {}
 
     Goblin(Game &game) : Creature(game, 1, 1) {}
 
-    int attack_modifier() override
-    {
-        return 0;
-    }
-
-    int defense_modifier() override
-    {
-        return 1;
-    }
-
     int get_attack() override {
-        // todo
-        return base_attack + game.attackModifier(this);
+        return get_statistic(StatQuery::attack);
     }
 
     int get_defense() override {
-        // todo
-        return base_defense + game.defenseModifier(this);
+        return get_statistic(StatQuery::defense);
+    }
+
+    void query(void *source, StatQuery &sq) override {
+        if (source == this)
+        {
+            switch (sq.statistic)
+            {
+            case StatQuery::attack:
+                sq.result += base_attack;
+                break;
+            case StatQuery::defense:
+                sq.result += base_defense;
+                break;
+            }
+        }
+        else
+        {
+            if (sq.statistic == StatQuery::defense)
+            {
+                sq.result++;
+            }
+        }
     }
 };
 
@@ -88,14 +80,11 @@ class GoblinKing : public Goblin
 public:
     GoblinKing(Game &game) : Goblin(game, 3, 3) {}
 
-    // todo
-    int attack_modifier() override
-    {
-        return 1;
-    }
-
-    int defense_modifier() override
-    {
-        return 1;
+    void query(void *source, StatQuery &sq) override {
+        if (source != this && sq.statistic == StatQuery::attack)
+        {
+            sq.result++;
+        }
+        else Goblin::query(source, sq);
     }
 };
